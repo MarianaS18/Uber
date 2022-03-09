08bed5283393b14cdfc786e58489c7b712e1b4ca
Mariana <mariana@fastmail.net>
2022-03-09 11:13:19 +0100

    project setup

---
+++ Uber/Controller/LoginController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginController.swift
+//  Uber
+//
+//  Created by Mariana Steblii on 09/03/2022.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+}
